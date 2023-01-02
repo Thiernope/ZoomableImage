b65bfb590627720fa5929d2e4b4be74cf6142a48
Thiernope <ntirandth@gmail.com>
2023-01-02 14:48:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+
 import './App.css';
+import firstImage from "./assets/imageOne.jpg";
 
 function App() {
+  const [yCord, setYCord] = useState();
+  const [xCord, setXCord] = useState();
+  const [state, setState] = useState(false);
+  const zoomPicture = (e) => {
+   const x = e.clientX - e.target.offsetLeft;
+   const y = e.clientY - e.target.offsetTop;
+   setState(true);
+   setYCord(y);
+   setXCord(x)
+  }
+
+  const restore= () => {
+    setState(false)
+  }
+
+  const inititalEffect = {
+    height: "100%",
+    width: "100%",
+    objectFit: "cover",
+    transform: 'scale(1)',
+    transformOrigin: "top",
+    cursor: 'pointer'
+  }
+  const hoveredEffect = {
+        height: "100%",
+        width: "100%",
+        objectFit: "cover",
+        transform: 'scale(2)',
+        transformOrigin: `${xCord}px ${yCord}px`,
+        cursor: 'pointer'
+   }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" onMouseMove={zoomPicture} onMouseLeave={restore}>
+        <img 
+        style={state === true? hoveredEffect: inititalEffect}
+        src={firstImage} alt="memories" id="imag"/>
+      </div>
     </div>
   );
 }
